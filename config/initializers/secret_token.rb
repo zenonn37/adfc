4323dca8616a9c533185ae5a2009cc84f183e452
chris crowd <zent29@gmail.com>
2013-11-03 14:37:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlbanydfcApp::Application.config.secret_key_base = '1ff04eb93cd9bf963e185e089b6e97b28fd0447e357753c95b69ef11751338fa7e0fa1231d9a8817613e9be40897f7a75becc284e87ca9ef3aee78ab266f01b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AlbanydfcApp::Application.config.session_store :cookie_store, key: '_albanydfc_app_session'
