cbd4ec9e8964b196ed44fb5a0cf99e24c1ceab4a
chris crowd <zent29@gmail.com>
2013-11-06 12:18:56 -0500

    mid-day

---
+++ app/assets/javascripts/details.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+
+    
+   //services code to reveal hidden details of services offered
+    $(".livingDetails").hover(function(){
+
+         $(".bdetails").slideDown();
+      
+
+    }, function(){
+
+       $(".bdetails").slideUp();
+
+    });
+
+
+
+     $(".kitchenDetails").hover(function(){
+
+         $(".kdetails").slideDown();
+      
+
+    }, function(){
+
+       $(".kdetails").slideUp();
+
+    });
+
+      $(".bathDetails").hover(function(){
+
+         $(".bathRoomDetails").slideDown();
+      
+
+    }, function(){
+
+       $(".bathRoomDetails").slideUp();
+
+    });
+
+
+
+       $(".bedDetails").hover(function(){
+
+         $(".bedRoomDetails").slideDown();
+      
+
+    }, function(){
+
+       $(".bedRoomDetails").slideUp();
+
+    });
+
+
+
+           $(".addDetails").hover(function(){
+
+         $(".adetails").slideDown();
+      
+
+    }, function(){
+
+       $(".adetails").slideUp();
+
+    });
+
+
+
+
+
+               $(".movingDetails").hover(function(){
+
+         $(".mdetails").slideDown();
+      
+
+    }, function(){
+
+       $(".mdetails").slideUp();
+
+    });
+
+
+
+
+
+
+
+
+
+
+});
