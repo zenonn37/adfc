ef7b70a00e71829d3c51b52ea7d06b90b4769145
chris crowd <zent29@gmail.com>
2013-11-06 15:24:17 -0500

    3pm commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,9 +2,41 @@ class ContactsController < ApplicationController
   def index
   end
 
+
+
   def new
+
+  	@contacts = Contact.new
+  end
+
+
+
+  def create
+  	@contacts = Contact.new(contact_params)
+
+  	if @contacts.save
+
+  		redirect_to static_pages url
+
+  	else
+
+  		render 'new'
+  	end
+  	
   end
 
   def show
+  	@contacts = Contact.params[:id]
   end
+
+
+  private
+
+  def contact_params
+  	params.require(:contact).permit(:name,:body,:email,:phone,:hours)
+  	
+  end
+
+
+
 end
